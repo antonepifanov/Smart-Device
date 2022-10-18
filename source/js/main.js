4d0a6132f44a0b11809fbe97d05e1a466c5ec5ff
antonepifanov <antonepifanov87@gmail.com>
2022-10-18 14:27:38 +0300

    Исправляет ошибки после ревью

--- source/js/main.js
+++ source/js/main.js
@@ -5,14 +5,10 @@ import {initPhoneMask} from './modules/init-phone-mask';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
-
   // Utils
-  // ---------------------------------
-
   iosVhFix();
 
   // Modules
-  // ---------------------------------
   initModals();
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
@@ -21,28 +17,3 @@ window.addEventListener('DOMContentLoaded', () => {
     initPhoneMask();
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
